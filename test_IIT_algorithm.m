7518c7d62d64f5e0663c334b0f6a8c7cff7fe832
卿欣 <zcfeng@buaa.edu.cn>
2022-12-14 13:11:04 +0100

    Update test_IIT_algorithm.m

--- test_IIT_algorithm.m
+++ test_IIT_algorithm.m
@@ -10,15 +10,16 @@ figure, imshow([S, C])
 pause(1)
 
 %% Intrinsic image transfer
+
 para.logarithm          =  1;
 para.color_transfer     =  0;
-para.bias               =  1e-3;
+para.bias               =  1/255; % for 8 bit image
 para.scale              =  1.0;
 para.color_space        =  'rgb'; 
 para.color_exemplar     =  'original';
 
 % paraetes for Gauss/Bilateral filter
-para.filter.k1            =  49;   
+para.filter.k1          =  49;   
 para.filter.delta_s     =  2.0;
 para.filter.delta_r     =  0.2;
 para.filter.mode        =  'gf';
@@ -30,9 +31,9 @@ para.LLE.k2             =  49;
 % Global paraetes
 para.alpha              =  0.9;
 para.beta               =  100;
-para.gamma           =  0.1;
+para.gamma              =  0.1;
 
-[T, M]                      =  IntrinsicImageTransfer(S,C,para);
-T                             = max(0,min(1,T));
+[T, M]                  =  IntrinsicImageTransfer(S,C,para);
+T                       = max(0,min(1,T));
 
-imshow(imresize([S C T],1,'nearest'))
+figure, imshow(imresize([S C T],1,'nearest'))
